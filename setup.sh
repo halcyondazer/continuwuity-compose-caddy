4f846ab0ed7809765a98d4c5387e45229ba00ed0
halcyondazer <halcyondazer@gmail.com>
2026-02-23 13:44:32 +0100

    i think this actually works

--- setup.sh
+++ setup.sh
@@ -6,12 +6,23 @@ if [[ -z "$1" ]]; then
    exit
 fi
 
-find ./continuwuity -type f -exec sed -i 's/yourdomain.tld/'"$1"'/g' {} \;
+STAMP=../continuwuity-$1-$(date +%s)
 
+cp -r continuwuity-skeleton $STAMP
+
+find $STAMP -type f -exec sed -i 's/yourdomain.tld/'"$1"'/g' {} \;
 KEYS=$(sudo docker run --rm livekit/livekit-server:latest generate-keys | cut -d ' ' -f 4)
-
 LIVEKIT_KEY=$(echo $KEYS | cut -d ' ' -f 1)
 LIVEKIT_SECRET=$(echo $KEYS | cut -d ' ' -f 2)
+find $STAMP -type f -exec sed -i 's/xxxxxxxxxxxxxxx/'"$LIVEKIT_KEY"'/g' {} \;
+find $STAMP -type f -exec sed -i 's/zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz/'"$LIVEKIT_SECRET"'/g' {} \;
+find $STAMP -type f -exec sed -i 's/yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy/'"$(openssl rand -hex 32)"'/g' {} \;
 
-find ./continuwuity -type f -exec sed -i 's/xxxxxxxxxxxxxxx/'"$LIVEKIT_KEY"'/g' {} \;
-find ./continuwuity -type f -exec sed -i 's/zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz/'"$LIVEKIT_SECRET"'/g' {} \;
+echo "your generated configuration is in the $STAMP folder."
+echo "do you want to start it now? enter 'yes'."
+read confirmation
+if [[ $confirmation = "yes" ]]; then
+   echo "starting the server using docker compose..."
+   cd $STAMP
+   docker compose up -d
+fi
