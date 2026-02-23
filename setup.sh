cfbd75e9d8d139d25e42476d7191fdb53a5aa1af
halcyondazer <halcyondazer@gmail.com>
2026-02-23 13:20:56 +0100

    dont worry, im less insane than you think... probably

--- setup.sh
+++ setup.sh
@@ -6,15 +6,12 @@ if [[ -z "$1" ]]; then
    exit
 fi
 
-find ./continuwuity -type f -exec sed 's/yourdomain.tld/'"$1"'/g' {} \;
+find ./continuwuity -type f -exec sed -i 's/yourdomain.tld/'"$1"'/g' {} \;
 
 KEYS=$(sudo docker run --rm livekit/livekit-server:latest generate-keys | cut -d ' ' -f 4)
 
 LIVEKIT_KEY=$(echo $KEYS | cut -d ' ' -f 1)
 LIVEKIT_SECRET=$(echo $KEYS | cut -d ' ' -f 2)
-echo $LIVEKIT_KEY
-echo $LIVEKIT_SECRET
-
 
 find ./continuwuity -type f -exec sed -i 's/xxxxxxxxxxxxxxx/'"$LIVEKIT_KEY"'/g' {} \;
-find ./continuwuity -type f -exec sed 's/zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz/'"$LIVEKIT_SECRET"'/g' {} \;
+find ./continuwuity -type f -exec sed -i 's/zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz/'"$LIVEKIT_SECRET"'/g' {} \;
